0fff3ff4db93c59953acd2418e65a6c48bc510bf
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-09-30 12:31:13 -0500

    local notification

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class HomePage extends StatelessWidget {
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  void showLocalNotification() {
+    _flutterLocalNotificationsPlugin.show(
+      0,
+      "HOLA DESDE FLUTTER",
+      "Este es el body desde Flutter",
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          "canal prueba id",
+          "canal prueba",
+          priority: Priority.max,
+          enableVibration: true,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +30,12 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Column(),
+        child: ElevatedButton(
+          child: Text("Notificación"),
+          onPressed: () {
+            showLocalNotification();
+          },
+        ),
       ),
     );
   }
