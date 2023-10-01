0e2ad9bfbadb72609ff95b8598c4fd8edc80259e
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-10-01 10:39:25 -0500

    init chart page

--- lib/pages/chart_page1.dart
+++ lib/pages/chart_page1.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:notificationsapp/models/data_model.dart';
 
 class ChartPage1 extends StatefulWidget {
   const ChartPage1({super.key});
@@ -8,6 +11,27 @@ class ChartPage1 extends StatefulWidget {
 }
 
 class _ChartPage1State extends State<ChartPage1> {
+  List<DataModel> _generateData(int max) {
+    final random = new Random();
+    return List.generate(
+      31,
+      (index) => DataModel(
+          date: DateTime(2023, 1, index + 1), value: random.nextDouble() * max),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    List<DataModel> lista = _generateData(10);
+    lista.forEach(
+      (element) {
+        print(element.date);
+        print(element.value);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
